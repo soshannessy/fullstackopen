66891d203a116ee383988b462d26ed2f757e72cc
soshannessy <samoshannessy@hotmail.com>
2024-05-30 14:35:28 +1000

    1.6: unicafe step 1

--- week1/unicafe/src/App.jsx
+++ week1/unicafe/src/App.jsx
@@ -1,15 +1,56 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      code here
+      button press history: {props.allClicks.join(' ')}
     </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat('G'))
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat('B'))
+    setBad(bad + 1)
+  }
+
+  return (
+      <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleGoodClick} text='good' /><Button handleClick={handleNeutralClick} text='neutral' /><Button handleClick={handleBadClick} text='bad' />
+      <h2>statistics</h2>
+      <p>good:{good}<br />neutral:{neutral}<br />bad:{bad}</p>
+
+      </div>
   )
 }
 
