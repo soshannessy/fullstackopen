f5857bb8ec00bdbcc816e248bee1475268d392ac
soshannessy <samoshannessy@hotmail.com>
2024-05-31 15:13:23 +1000

    Finished anecdotes

--- week1/anecdotes/src/App.jsx
+++ week1/anecdotes/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getMaxOfArray(numArray) {
+  let max = numArray[0];
+  for (let i = 1; i < numArray.length; i++) {
+    if (numArray[i] > max) {
+      max = numArray[i];
+    }
+  }
+  return max;
+}
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-)
-
-const randomNumber = {
-  return Math.random(anecdotes.length);
-}
+);
 
 const App = () => {
   const anecdotes = [
@@ -21,13 +33,34 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+  
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0)
+
+  const handleRandomGen = () => {
+    const random = getRandomInt(0,anecdotes.length - 1)
+    setSelected(random)
+  }
+
+  const handleVote = (quote) => {
+    const newVotes = [...votes];
+    newVotes[quote] += 1;
+    setVotes(newVotes);
+  }
+
+  const maxVotes = getMaxOfArray(votes);
+  console.log(maxVotes)
+  const maxVotesIndex = votes.indexOf(maxVotes);
+  console.log(maxVotesIndex)
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>,
-      <Button handleClick="randomNumber" text="next anecdote"/>
+      <h2>Anecdote of the Day</h2>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => handleVote(selected)} text='Vote'/>
+      <Button handleClick={handleRandomGen} text='Next Anecdote'/>
+      <h2>Anecdote with the most votes</h2>
+      <p>{anecdotes[maxVotesIndex]}</p>
     </div>
   )
 }
