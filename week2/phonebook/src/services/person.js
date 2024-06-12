629ae3e541ef83a8d7bedae45c99ddd0a5a3c1ba
soshannessy <samoshannessy@hotmail.com>
2024-06-12 11:59:17 +1000

    files prepping for deployment on seperate git repo

--- week2/phonebook/src/services/person.js
+++ week2/phonebook/src/services/person.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
-  return axios.get(baseUrl).then(response => response.data);
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
 };
 
 const create = newObject => {
-  return axios.post(baseUrl, newObject).then(response => response.data);
+  const request = axios.post(baseUrl, newObject);
+  return request.then(response => response.data);
 };
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data);
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response => response.data);
 };
 
 const remove = id => {
   if (window.confirm('Do you want to delete this person?')) {
-    return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
+    const request = axios.delete(`${baseUrl}/${id}`);
+    return request.then(response => response.data);
   }
   return Promise.resolve();
 };
